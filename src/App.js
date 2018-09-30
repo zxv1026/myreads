31c0f50bd9d50d5de9abe1406805a277f34b6c7d
张晓钒 <zxv970209@outlook.com>
2018-09-30 22:03:42 +0800

    feature：新增Book组件，并用BooksAPI获取数据来显示图书

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from "react-router-dom";
 import './App.css'
 import SearchBooks from "./SearchBooks";
 import ListBooks from "./ListBooks";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    }
+    console.log("app", this)
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
@@ -23,7 +27,9 @@ class BooksApp extends React.Component {
           <SearchBooks />
         )}/>
         < Route exact path = "/" render={() => (
-          <ListBooks />
+          <ListBooks 
+            books = {this.state.books}
+          />
         )}/>
       </div>
     )
---
+++ src/Book.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.authors}</div>
+            </div>
+        )
+    }
+}
+
+export default Book
